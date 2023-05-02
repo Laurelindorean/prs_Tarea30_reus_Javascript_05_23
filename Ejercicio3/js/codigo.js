5d859ff7318d2d70cc9dc7476ef56e4f0674291a
Palmira Romia <kguinera@hotmail.com>
2023-05-02 12:16:25 +0200

    Task 3 complete

---
+++ Ejercicio3/js/codigo.js
@@ -0,0 +1,28 @@
+//Creamos un array con los meses del año
+let meses = [
+  "enero",
+  "febrero",
+  "marzo",
+  "abril",
+  "mayo",
+  "junio",
+  "julio",
+  "agosto",
+  "septiembre",
+  "octubre",
+  "noviembre",
+  "diciembre",
+];
+//Mostramos por consola los meses del año
+console.log(meses[0]);
+console.log(meses[1]);
+console.log(meses[2]);
+console.log(meses[3]);
+console.log(meses[4]);
+console.log(meses[5]);
+console.log(meses[6]);
+console.log(meses[7]);
+console.log(meses[8]);
+console.log(meses[9]);
+console.log(meses[10]);
+console.log(meses[11]);
