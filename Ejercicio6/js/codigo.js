dd733fbd7b528597d1cf4b9e9c64efdb7e1a8b50
Palmira Romia <kguinera@hotmail.com>
2023-05-02 13:41:42 +0200

    Task 6 complete

---
+++ Ejercicio6/js/codigo.js
@@ -0,0 +1,41 @@
+let letras = [
+  "T",
+  "R",
+  "W",
+  "A",
+  "G",
+  "M",
+  "Y",
+  "F",
+  "P",
+  "D",
+  "X",
+  "B",
+  "N",
+  "J",
+  "Z",
+  "S",
+  "Q",
+  "V",
+  "H",
+  "L",
+  "C",
+  "K",
+  "E",
+  "T",
+];
+
+let numero = prompt("Escribe tu DNI sin la letra");
+let letra = prompt("Escribe la letra de tu DNI").toUpperCase(); //pasamos la letra a mayusculas
+
+if (numero < 0 || numero > 99999999) {
+  alert("El número proporcionado no es válido");
+} else {
+  let resto = numero % 23;
+  let letraSel = letras[resto];
+  if (letra != letraSel) {
+    alert("La letra indicada no es correcta");
+  } else {
+    alert("El numero + " + numero + " y  la letra " + letra + " son correctos");
+  }
+}
